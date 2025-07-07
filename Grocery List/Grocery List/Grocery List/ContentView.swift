0ee5636f39eb8875a8a54dc3dde3612ec6a2505d
Josh Gdovin <ios@gdovindesigns.com>
2025-07-07 10:10:38 -0400

    added keyboard

--- Grocery List/Grocery List/Grocery List/ContentView.swift
+++ Grocery List/Grocery List/Grocery List/ContentView.swift
@@ -13,6 +13,10 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     
+    @State private var item: String = ""
+    
+    @FocusState private var isFocused: Bool
+    
     func addEssentialFood() {
         modelContext.insert(Item(title: "Bakery & Bread", isCompleted: false))
         modelContext.insert(Item(title: "Meat & Seafood", isCompleted: true))
@@ -31,6 +35,21 @@ struct ContentView: View {
                         .foregroundStyle(item.isCompleted == false ? Color.primary : Color.accentColor)
                         .strikethrough(item.isCompleted)
                         .italic(item.isCompleted)
+                        .swipeActions{
+                            Button(role: .destructive) {
+                                withAnimation {
+                                    modelContext.delete(item)
+                                }
+                            } label: {
+                                Label("Delete", systemImage:"trash")
+                            }
+                        }
+                        .swipeActions(edge: .leading) {
+                            Button("Done", systemImage: item.isCompleted == false ? "checkmark.circle" : "x.circle"){
+                                item.isCompleted.toggle()
+                            }
+                            .tint(item.isCompleted == false ? .green: .accentColor)
+                        }
                 }
             }
             .navigationTitle("Grocery List")
@@ -39,8 +58,8 @@ struct ContentView: View {
                     ToolbarItem(placement: .topBarTrailing) {
                         Button{
                             addEssentialFood()
-                        }label {
-                            Label
+                        }label: {
+                            Label("Essentials", systemImage: "carrot")
                         }
                     }
                 }
@@ -50,6 +69,37 @@ struct ContentView: View {
                     ContentUnavailableView("Empty Cart", systemImage: "cart.circle", description: Text("Add some items to the shopping list"))
                 }
             }
+            .safeAreaInset(edge: .bottom) {
+                VStack(spacing: 12) {
+                    TextField("", text:$item)
+                        .textFieldStyle(.plain)
+                        .padding(12)
+                        .background(.tertiary)
+                        .cornerRadius(12)
+                        .font(.title.weight(.light))
+                        .focused($isFocused)
+                    
+                    Button {
+                        //doesn't allowe empty entries
+                        guard !item.isEmpty else { return }
+                        
+                        let newItem = Item(title: item, isCompleted: false)
+                        modelContext.insert(newItem)
+                        item = ""
+                        isFocused = false
+                    }label: {
+                        Text("Save")
+                            .font(.title2.weight(.medium))
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle)
+                    .controlSize(.extraLarge)
+                }
+                .padding()
+                .background(.bar)
+            }
+            
         }
     }
 }
