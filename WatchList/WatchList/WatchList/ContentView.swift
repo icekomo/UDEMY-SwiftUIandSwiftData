f9da336b12eadbba201b822702a4da9ef4ad3223
Josh Gdovin <ios@gdovindesigns.com>
2025-07-25 15:20:58 -0400

    added new project

---
+++ WatchList/WatchList/WatchList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchList
+//
+//  Created by Josh Gdovin on 7/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchList/WatchList/WatchList/WatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchListApp.swift
+//  WatchList
+//
+//  Created by Josh Gdovin on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
