676282357414fdd9f23c68fd286b39512fbd78eb
Josh Gdovin <ios@gdovindesigns.com>
2025-07-27 23:28:52 -0400

    completed app

---
+++ WatchList/WatchList/WatchList/View/NewMovieFormView.swift
@@ -0,0 +1,88 @@
+//
+//  NewMovieFormView.swift
+//  WatchList
+//
+//  Created by Josh Gdovin on 7/27/25.
+//
+
+import SwiftUI
+
+struct NewMovieFormView: View {
+    //MARK: - PROPERTIES
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var title: String = ""
+    @State private var selectedGenre: Genre = .kids
+    
+    //MARK: - FUNCTIONS
+    private func addNewMovie(){
+        //adds values to the movie that is being created
+        let newMovie = Movie(title: title, genre: selectedGenre)
+        // add the movie to SwiftData
+        modelContext.insert(newMovie)
+        // resets the input and picker
+        title = ""
+        selectedGenre = .kids
+    }
+    
+    var body: some View {
+        Form {
+            List{
+                //MARK: - HEADER
+                Text("What to Watch")
+                    .font(.largeTitle.weight(.black))
+                    .foregroundStyle(.blue.gradient)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                    .padding(.vertical)
+                
+                //MARK: - TITLE
+                TextField("Movie Title", text: $title)
+                    .textFieldStyle(.roundedBorder)
+                    .font(.largeTitle.weight(.light))
+                
+                //MARK: - GENRE
+                Picker("Genre", selection: $selectedGenre) {
+                    ForEach(Genre.allCases) { genre in
+                        Text(genre.name)
+                            .tag(genre)
+                    }
+                }
+                
+                //MARK: - SAVE BUTTON
+                Button {
+                    if title.isEmpty || title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                        print("Input is invalid")
+                        return
+                    } else {
+                        print("Valid input: \(title) - \(selectedGenre)")
+                        addNewMovie()
+                        dismiss()
+                    }
+                } label :{
+                    Text("Save")
+                        .font(.title2.weight(.medium))
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent)
+                .controlSize(.extraLarge)
+                .buttonBorderShape(.roundedRectangle)
+                .disabled(title.isEmpty || title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
+                
+                //MARK: = CANCLE BUTTON
+                Button {
+                    dismiss()
+                }label : {
+                    Text("Close")
+                        .frame(maxWidth: .infinity)
+                }
+                
+            }//: LIST
+            .listRowSeparator(.hidden)
+        }//: FORM
+    }
+}
+
+#Preview {
+    NewMovieFormView()
+}
